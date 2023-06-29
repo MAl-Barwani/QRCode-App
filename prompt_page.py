881a3aa180ab1fb5a6fda222f08440b981b2dc38
Muhammad Al-Barwani <mohammed.barwani@outlook.com>
2023-06-29 09:36:57 +0400

    modified:   .gitignore 	modified:   prompt_page.py 	modified:   qrcode.png 	modified:   result_page.py

--- prompt_page.py
+++ prompt_page.py
@@ -1,45 +1,39 @@
-import qrcode 
+import qrcode
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.app import App
 from result_page import ResultPage
-from kivy.uix.image import Image
+
 
 class PromptPage(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        # Decide the number of columns 
+        # Decide the number of columns
         self.cols = 1
 
         # Label for instructions
-        self.add_widget(Label(text='Paste the link you want to\n convert in the box below.', font_size=20))
+        self.add_widget(Label(
+            text='Paste the link you want to\n convert in the box below.', font_size=20))
 
         # Add TextInput for pasting the link
-        self.textinput = TextInput(multiline=False, size_hint=(1,0.2))
+        self.textinput = TextInput(multiline=False, size_hint=(1, 0.2))
         self.add_widget(self.textinput)
-
- 
 
         # Convert Button
         self.button = Button(text='Convert to QRCode', font_size=20)
         self.button.bind(on_press=self.generate_qr_code)
         self.add_widget(self.button)
-        
+
     # What happens when I press convert
     def generate_qr_code(self, instance):
         # Generate the QR Code
         link = self.textinput.text
         img = qrcode.make(link)
         img.save('qrcode.png')
-        #Reload next scene's QRCode
+        # Reload next scene's QRCode
         ResultPage.image.reload()
         # Move to the results page
         App.get_running_app().root.current = 'ResultPage'
-
-
-
-
-        
--- result_page.py
+++ result_page.py
@@ -3,10 +3,11 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.app import App
-import os
+
 
 class ResultPage(GridLayout):
     image = Image(source='qrcode.png', fit_mode='scale-down')
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.cols = 1
@@ -16,15 +17,13 @@ class ResultPage(GridLayout):
 
         # Label to screenshot
 
-        self.add_widget(Label(text='Screenshot this QRCode to save it to your device.'))
+        self.add_widget(
+            Label(text='Screenshot this QRCode to save it to your device.'))
 
         # Generate Another Code Button
         self.button = Button(text='Generate Another QRCode')
         self.button.bind(on_press=self.go_back)
         self.add_widget(self.button)
-    
+
     def go_back(self, instance):
         App.get_running_app().root.current = 'PromptPage'
-
-        
-
