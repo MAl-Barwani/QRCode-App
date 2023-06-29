5ff63c809442623e162ae79144a652686299a078
Muhammad Al-Barwani <mohammed.barwani@outlook.com>
2023-06-29 09:33:14 +0400

    modified:   main_app.py 	modified:   prompt_page.py 	new file:   result_page.py

--- main_app.py
+++ main_app.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
 from prompt_page import PromptPage
+from result_page import ResultPage
+
 
 class MainApp(App):
     def build(self):
@@ -11,8 +13,14 @@ class MainApp(App):
         screen.add_widget(self.prompt_page)
         self.screen_manager.add_widget(screen)
 
+        # Load the result page
+        screen = Screen(name='ResultPage')
+        self.result_page = ResultPage()
+        screen.add_widget(self.result_page)
+        self.screen_manager.add_widget(screen)
+
         return self.screen_manager
-    
-    
+
+
 if __name__ == "__main__":
     MainApp().run()
--- prompt_page.py
+++ prompt_page.py
@@ -3,7 +3,9 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-
+from kivy.app import App
+from result_page import ResultPage
+from kivy.uix.image import Image
 
 class PromptPage(GridLayout):
     def __init__(self, **kwargs):
@@ -13,24 +15,29 @@ class PromptPage(GridLayout):
         self.cols = 1
 
         # Label for instructions
-        self.add_widget(Label(text='Paste the link you want to\n convert in the box below.'))
+        self.add_widget(Label(text='Paste the link you want to\n convert in the box below.', font_size=20))
 
         # Add TextInput for pasting the link
         self.textinput = TextInput(multiline=False, size_hint=(1,0.2))
         self.add_widget(self.textinput)
 
-        
+ 
 
         # Convert Button
-        self.button = Button(text='Convert')
+        self.button = Button(text='Convert to QRCode', font_size=20)
         self.button.bind(on_press=self.generate_qr_code)
         self.add_widget(self.button)
         
     # What happens when I press convert
     def generate_qr_code(self, instance):
+        # Generate the QR Code
         link = self.textinput.text
         img = qrcode.make(link)
         img.save('qrcode.png')
+        #Reload next scene's QRCode
+        ResultPage.image.reload()
+        # Move to the results page
+        App.get_running_app().root.current = 'ResultPage'
 
 
 
